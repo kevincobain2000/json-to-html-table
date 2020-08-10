426a448bfd0f9f6f73b8130158d6c235e2961b30
Pulkit Kathuria <kevincobain2000@gmail.com>
2020-08-10 19:03:54 +0900

    allow wrapper classname

--- src/index.js
+++ src/index.js
@@ -2,32 +2,34 @@ import React from 'react'
 import styles from './styles.css'
 
 export const JSONToHTMLTable = (props) => {
-  const { data, tableClassName } = props
+  const { data, wrapperClassName, tableClassName } = props
   return (
-    <table className={tableClassName}>
-      <tbody>
-        {Object.keys(data).map((k) => (
-          <tr key={k}>
-            {!Array.isArray(data)
-              && <td>{k.replace(/_/g, ' ')}</td>}
-            {(() => {
-              if (data[k] && typeof data[k] === 'object') {
+    <div className={wrapperClassName}>
+      <table className={tableClassName}>
+        <tbody>
+          {Object.keys(data).map((k) => (
+            <tr key={k}>
+              {!Array.isArray(data)
+                && <td>{k.replace(/_/g, ' ')}</td>}
+              {(() => {
+                if (data[k] && typeof data[k] === 'object') {
+                  return (
+                    <td>
+                      <JSONToHTMLTable data={data[k]} tableClassName={tableClassName} />
+                    </td>
+                  )
+                }
                 return (
                   <td>
-                    <JSONToHTMLTable data={data[k]} tableClassName={tableClassName} />
+                    <span dangerouslySetInnerHTML={{ __html: data[k] }} />
                   </td>
                 )
-              }
-              return (
-                <td>
-                  <span dangerouslySetInnerHTML={{ __html: data[k] }} />
-                </td>
-              )
-            })()}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+              })()}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
